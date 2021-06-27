874c9c5de9f1143b9a54ae787b43d42abe76280f
Asari Mitsuteru <asari.mtr@gmail.com>
2021-06-27 19:42:20 +0900

    dynamic import

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
-function getComponent() {
+async function getComponent() {
     const element = document.createElement('div');
 
-    return import('lodash')
-        .then(({ default: _ }) => {
-            const element = document.createElement('div');
+    const { default: _ } = await import('lodash');
 
-            element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-            return element;
-        })
-        .catch((error) => 'An error occurred while loading the component');
+    return element;
 }
 
 getComponent.then((component) => {
