ca16a034970ff51b309a863f6548dc0d3ef016b7
Asari Mitsuteru <asari.mtr@gmail.com>
2021-06-27 19:34:13 +0900

    entry dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,15 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     mode: 'development',
     entry: {
-        index: './src/index.js',
-        another: './src/another-module.js',
+        index: {
+            import: './src/index.js',
+            dependOn: 'shared',
+        },
+        another: {
+            import: './src/another-module.js',
+            dependOn: 'shared',
+        },
+        shared: 'lodash',
     },
     devtool: 'inline-source-map',
     devServer: {
@@ -19,5 +26,9 @@ module.exports = {
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
+        clean: true,
     },
+    optimization: {
+        runtimeChunk: 'single',
+    }
 };
